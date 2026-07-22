4d7793b3734326bc6b21c2ae3b9d2de1f42c4986
Paradox <123134773+AbhiWiryateja2201@users.noreply.github.com>
2026-07-22 15:31:23 +0700

    Sambung API

---
+++ Backend/iwallet/src/main/java/com/IWallet/iwallet/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.IWallet.iwallet.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:5173") // Izinkan React (Vite) mengakses API ini
+public class TestController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Halo dari Spring Boot Backend!";
+    }
+}
