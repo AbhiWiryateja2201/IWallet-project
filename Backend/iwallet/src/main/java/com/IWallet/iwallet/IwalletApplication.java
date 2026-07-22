f130576dbcbc80a03e4207e0af49c016a381c22a
Paradox <123134773+AbhiWiryateja2201@users.noreply.github.com>
2026-07-22 15:12:24 +0700

    Update Directory lagi

---
+++ Backend/iwallet/src/main/java/com/IWallet/iwallet/IwalletApplication.java
@@ -0,0 +1,13 @@
+package com.IWallet.iwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwalletApplication.class, args);
+	}
+
+}
